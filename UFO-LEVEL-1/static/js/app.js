536348f5cbd70a062aaa2d79704186cd6d3b860c
Wes90 <70103986+Wes90@users.noreply.github.com>
2020-12-30 17:41:28 -0700

    checkpoint

--- UFO-LEVEL-1/static/js/app.js
+++ UFO-LEVEL-1/static/js/app.js
@@ -8,8 +8,17 @@ data.forEach(function(UFOreport) {
     var row = tbody.append('tr');
     Object.entries(UFOreport).forEach(function([key, value]) {
         row.append('td').text(value);
-    })
+    });
 });
 
 
-    
+var inputfield = d3.select('#datetime');
+var button = d3.select('#filter-btn');
+
+
+
+function filterreport(){
+    console.log(d3.event.target);
+}
+inputfield.on("enter", filterreport);  
+button.on('click', filterreport);
