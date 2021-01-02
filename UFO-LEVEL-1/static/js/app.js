1639d7e305137008149ad2c22c66ef996ef9a236
Wes90 <70103986+Wes90@users.noreply.github.com>
2021-01-02 01:09:01 -0700

    checkpoint

--- UFO-LEVEL-1/static/js/app.js
+++ UFO-LEVEL-1/static/js/app.js
@@ -4,7 +4,7 @@ var tableData = data;
 // YOUR CODE HERE!
 var tbody = d3.select("tbody");
 
-data.forEach(function(UFOreport) {
+tableData.forEach(function(UFOreport) {
     var row = tbody.append('tr');
     Object.entries(UFOreport).forEach(function([key, value]) {
         row.append('td').text(value);
@@ -12,13 +12,18 @@ data.forEach(function(UFOreport) {
 });
 
 
-var inputfield = d3.select('#datetime');
+var inputfield = d3.select('.form-control').text();
 var button = d3.select('#filter-btn');
 
 
 
 function filterreport(){
+    for (const [key, value] of Object.entries(UFOreport)) {
+        console.log(`${key}: ${value}`);
+      }
+    
+    console.log("A button was clicked!");
     console.log(d3.event.target);
-}
-inputfield.on("enter", filterreport);  
+    console.log(inputfield);
+}  
 button.on('click', filterreport);
